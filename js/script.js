91c73ac12d964859756aef13c534fc0671004367
Alexandre-Guilbert <a.guilbert@labo-ve.fr>
2024-10-16 10:40:01 +0200

    1er envoi de documents

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@
 function lancerDe() {
     // Lance un dé de 20 faces (résultat entre 1 et 20)
     let resultat = Math.floor(Math.random() * 20) + 1;
-    console.log("Résultat du lancer : " + resultat);
     // Affiche le résultat du lancer dans l'élément avec l'id "resultat-de"
     document.getElementById("resultat-de").textContent = "Résultat du lancer : " + resultat;
     // Cache toutes les possibilités
@@ -25,10 +24,7 @@ function cacherBouton() {
 }
 //Galerie d'images JS //
 const galleryItems = document.querySelectorAll('.gallery-item');
-const modal = document.getElementById('modal');
-const modalImg = document.getElementById('img01');
 const captionText = document.getElementById('caption');
-const closeBtn = document.querySelector('.close');
 
 galleryItems.forEach(item => {
     item.onclick = function() {
@@ -37,11 +33,6 @@ galleryItems.forEach(item => {
         captionText.innerHTML = this.alt;
     }
 });
-
-closeBtn.onclick = function() {
-    modal.style.display = "none";
-}
-
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
